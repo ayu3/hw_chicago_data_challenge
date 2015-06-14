f78307f6f03c6e84b78075ffce134a2910f80811
Amy Yu <ayu3@luc.edu>
2015-06-14 01:05:01 -0500

    working on reading csv files using CsvHelper

---
+++ FileReader.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using CsvHelper;
+
+namespace hw2_chicago_data_challenge
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			
+		}
+	}
+}
---
+++ PublicHealthData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using CsvHelper;
+
+/*trying a tutorial from 
+ * http://carlosferreira.com/reading-csv-files-using-csvhelper-package-c-ienumerable/
+ * on how to use CvsHelper
+*/
+
+namespace hw2_chicago_data_challenge
+{
+	public class PublicHealthData
+	{
+		public String ComAreaNumber { get; set; }
+		public String ComArea { get; set; }
+		public String LifeExp1990 { get; set; }
+		public String Lower1990 { get; set; }
+		public String Upper1990 { get; set; }
+		public String LifeExp2000 { get; set; }
+		public String Lower2000 { get; set; }
+		public String Upper2000 { get; set; }
+		public String LifeExp2010 { get; set; }
+		public String Lower2010 { get; set; }
+		public String Upper2010 { get; set; }
+	}
+}
