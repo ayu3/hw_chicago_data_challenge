58d7c53761b986e30a4c9f666e97b5649698ca19
Amy Yu <ayu3@luc.edu>
2015-06-15 00:12:59 -0500

    still trying to get a good grasp on CsvHelper. Should have gotten the code for reading the file correctly (have only written it for the first csv file), now trying to figure out how to store the data I need from the reader.

--- FileReader.cs
+++ FileReader.cs
@@ -1,13 +1,25 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
 using CsvHelper;
+using CsvHelper.Configuration;
+using CsvHelper.TypeConversion;
 
 namespace hw2_chicago_data_challenge
 {
-	class MainClass
+	class FileReader
 	{
 		public static void Main (string[] args)
 		{
-			
+			//reader
+			using (var sr = new StreamReader (@"Public_Health_Statistics-_Life_Expectancy_By_Community_Area.csv")) {
+				var reader = new CsvReader (sr);
+				reader.Configuration.RegisterClassMap<PublicHealthMap> ();
+				while (reader.Read ()) {
+					var record = reader.GetRecord<PublicHealthDataDef> ();
+				}
+			}
 		}
 	}
 }
--- PublicHealthData.cs
+++ PublicHealthData.cs
@@ -1,15 +1,32 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
+using System.Collections.Generic;
 using CsvHelper;
-
-/*trying a tutorial from 
- * http://carlosferreira.com/reading-csv-files-using-csvhelper-package-c-ienumerable/
- * on how to use CvsHelper
-*/
+using CsvHelper.Configuration;
+//using CsvHelper.TypeConversion;
 
 namespace hw2_chicago_data_challenge
 {
-	public class PublicHealthData
+	public sealed class PublicHealthMap : CsvClassMap<PublicHealthDataDef>
+	{
+		public PublicHealthMap()
+		{
+			Map (m => m.ComAreaNumber).Name ("Community Area Number");
+			Map (m => m.ComArea).Name ("Community Area");
+			Map (m => m.LifeExp1990).Name ("1990 Life Expectancy");
+			Map (m => m.Lower1990).Name ("1990 Lower 95% CI");
+			Map (m => m.Upper1990).Name ("1990 Upper 95% CI");
+			Map (m => m.LifeExp2000).Name ("2000 Life Expectancy");
+			Map (m => m.Lower2000).Name ("2000 Lower 95% CI");
+			Map (m => m.Upper2000).Name ("2000 Upper 95% CI");
+			Map (m => m.LifeExp2010).Name ("2010 Life Expectancy");
+			Map (m => m.Lower2010).Name ("2010 Lower 95% CI");
+			Map (m => m.Upper2010).Name ("2010 Upper 95% CI");
+		}
+	}
+
+	public sealed class PublicHealthDataDef
 	{
 		public String ComAreaNumber { get; set; }
 		public String ComArea { get; set; }
